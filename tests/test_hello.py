3eed4e01472d3ba30f12f8ce55ab97fc235b9eea
Leonardo Damasceno <leonardo.damasceno@hu.com.br>
2018-09-21 14:45:25 -0300

    installed lint and fixed formatting

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,14 +1,18 @@
 import pytest
 from app import app
 
+
 @pytest.fixture
 def resp():
-  client = app.test_client()
-  response = client.get('/')
-  return response
+    client = app.test_client()
+    response = client.get('/')
+    return response
+
+
 def test_root_status_code(resp):
-  """Testing status code from root"""
-  assert resp.status_code == 200
+    """Testing status code from root"""
+    assert resp.status_code == 200
+
 
 def test_root_message(resp):
-  assert "Hello World" in resp.get_data(as_text=True)
+    assert "Hello World" in resp.get_data(as_text=True)
